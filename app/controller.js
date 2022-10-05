193040c864df9656037a5cfd53e628fd4e9ac386
Ethan Weiner <ethanweiner20@gmail.com>
2022-10-05 19:57:48 -0400

    Controller starter logic

--- app/controller.js
+++ app/controller.js
@@ -2,7 +2,12 @@ class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+    this.view.bindButtonClick(this.handleCalculatorClick);
   }
+
+  handleCalculatorClick = (type, symbol) => {
+    this.model.input(type, symbol);
+  };
 }
 
 export default Controller;
