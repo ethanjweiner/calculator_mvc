4fc0626089efe39a1049d28a178fb0e7bf10bbd3
Ethan Weiner <ethanweiner20@gmail.com>
2022-10-05 19:56:01 -0400

    Begin view logic

--- app/view.js
+++ app/view.js
@@ -1,3 +1,39 @@
-class View {}
+class View {
+  constructor() {
+    this.entryWindow = document.querySelector('#entry-window');
+    this.operationWindow = document.querySelector('#operation-window');
+    this.buttonsContainer = document.querySelector('#buttons');
+    this.clear();
+  }
+
+  // Window updates
+  setEntry(entry) {
+    this.entryWindow.textContent = entry;
+  }
+
+  setOperation(operation) {
+    this.operationWindow.textContent = operation;
+  }
+
+  clear() {
+    this.setOperation('');
+    this.clearEntry();
+  }
+
+  clearEntry() {
+    this.setEntry('0');
+  }
+
+  // Bind handlers to click event
+  bindButtonClick(handler) {
+    this.buttonsContainer.addEventListener('click', (e) => {
+      const target = e.target;
+
+      if (target instanceof HTMLButtonElement) {
+        handler(target.dataset.type, target.dataset.symbol);
+      }
+    });
+  }
+}
 
 export default View;
