57c96722e69b7a7185dfdf0f541bbb8f3459cdd8
Ethan Weiner <ethanweiner20@gmail.com>
2022-10-05 16:18:36 -0400

    Page structure and styles

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ import Controller from './app/controller.js';
 import Model from './app/model.js';
 import View from './app/view.js';
 
-const app = new Controller(new Model(), new View());
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Controller(new Model(), new View());
+});
