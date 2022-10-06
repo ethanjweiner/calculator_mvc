a89373ae82f73a7240a5fba57323e0fb67ed9290
Ethan Weiner <ethanweiner20@gmail.com>
2022-10-05 20:16:12 -0400

    Setup model

--- app/model.js
+++ app/model.js
@@ -1,3 +1,32 @@
-class Model {}
+class Model {
+  constructor() {
+    this.operation = [];
+    this.entry = 0;
+    this.result = null; // Necessary?
+  }
+
+  // Create dispatchers on changes
+  bindEntryChanged(callback) {
+    this.onEntryChange = callback;
+  }
+
+  bindOperationChanged() {
+    this.onOperationChange = callback;
+  }
+
+  inputDigit(digit) {}
+
+  clearEntry() {}
+
+  clear() {}
+
+  runOperator(operator) {}
+
+  negate() {}
+
+  addDecimal() {}
+
+  getResult() {}
+}
 
 export default Model;
